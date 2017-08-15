c5525bbb8534165045ebc15ed413ddd844893278
Derek Stavis <dekestavis@gmail.com>
2017-08-15 19:55:51 -0300

    chore: add initial project structure

---
+++ webapp/src/components/Button/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Button = ({ children }) => (
+  <button>
+    {children}
+  </button>
+)
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+}
+
+export default Button
+
