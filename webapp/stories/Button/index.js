c3802aa47484c14fca7a29f3c2f435d1fe0cd447
Derek W. Stavis <dekestavis@gmail.com>
2017-09-02 18:17:02 -0300

    button: refactor interface, simplify orgnization
    This refactors Button interface and reorganizes the file architecture.
    
    - Rename `theme` prop to `base`
    
      Theme is a prop name that can be used to replace the whole theme when
      using `react-themr`, for example.
    
    - Rename `style` prop to `variant`
    
      This was necessary because `style` is a HTML reserved keyword and
      a) eslint was complaining about b) can tripple users.
    
    - Consolidate CSS custom properties into `properties.css`
    
      This is more in line with Card component. This name standard should
      also be in our React styleguide!
    
    - Rename color classes from camel case to dash case, keeping in line with CSS

--- webapp/stories/Button/index.js
+++ webapp/stories/Button/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-import ButtonMyAccount from 'react-icons/lib/md/face'
-import ButtonLogoff from 'react-icons/lib/fa/power-off'
-import ButtonExport from 'react-icons/lib/fa/external-link'
-import ButtonCopy from 'react-icons/lib/md/content-copy'
-import ButtonDelete from 'react-icons/lib/fa/close'
-import ButtonAdd from 'react-icons/lib/fa/plus'
-import ButtonDocs from 'react-icons/lib/md/import-contacts'
-import ButtonLetter from 'react-icons/lib/md/assignment'
-import ButtonUpload from 'react-icons/lib/md/file-upload'
+import IconMyAccount from 'react-icons/lib/md/face'
+import IconLogoff from 'react-icons/lib/fa/power-off'
+import IconExport from 'react-icons/lib/fa/external-link'
+import IconCopy from 'react-icons/lib/md/content-copy'
+import IconDelete from 'react-icons/lib/fa/close'
+import IconAdd from 'react-icons/lib/fa/plus'
+import IconDocs from 'react-icons/lib/md/import-contacts'
+import IconLetter from 'react-icons/lib/md/assignment'
+import IconUpload from 'react-icons/lib/md/file-upload'
 
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
@@ -18,8 +18,8 @@ import stylesheet from './style.css'
 
 const i18n = {
   Button: 'Botão',
-  Light: 'Claro',
-  Dark: 'Escuro',
+  Light: 'Base Clara',
+  Dark: 'Base Escura',
   MyAccount: 'Minha Conta',
   Logoff: 'Desconectar',
   Export: 'Exportar',
@@ -41,14 +41,14 @@ const i18n = {
 }
 
 const parentList = [
-  { theme: 'light', name: i18n.Light, style: { padding: '20px', background: '#FFF' } },
-  { theme: 'dark', name: i18n.Dark, style: { padding: '20px', background: '#333' } },
+  { base: 'light', name: i18n.Light, style: { padding: '20px', background: '#FFF' } },
+  { base: 'dark', name: i18n.Dark, style: { padding: '20px', background: '#333' } },
 ]
 
 const colorList = [
-  'greenPrimary',
-  'greenSecondary',
-  'greenContrast',
+  'green-primary',
+  'green-secondary',
+  'green-contrast',
   'silver',
   'plumb',
   'yellow',
@@ -59,15 +59,15 @@ const colorList = [
 ]
 
 const iconList = [
-  { component: ButtonMyAccount, text: i18n.MyAccount },
-  { component: ButtonLogoff, text: i18n.Logoff },
-  { component: ButtonExport, text: i18n.Export },
-  { component: ButtonCopy, text: i18n.Copy },
-  { component: ButtonDelete, text: i18n.Delete },
-  { component: ButtonAdd, text: i18n.Add },
-  { component: ButtonDocs, text: i18n.Docs },
-  { component: ButtonLetter, text: i18n.Letter },
-  { component: ButtonUpload, text: i18n.Upload },
+  { component: IconMyAccount, text: i18n.MyAccount },
+  { component: IconLogoff, text: i18n.Logoff },
+  { component: IconExport, text: i18n.Export },
+  { component: IconCopy, text: i18n.Copy },
+  { component: IconDelete, text: i18n.Delete },
+  { component: IconAdd, text: i18n.Add },
+  { component: IconDocs, text: i18n.Docs },
+  { component: IconLetter, text: i18n.Letter },
+  { component: IconUpload, text: i18n.Upload },
 ]
 
 const sizeList = [
@@ -76,57 +76,72 @@ const sizeList = [
 
 const clicked = action('clicked')
 
+function buttonAllColors (parent, children, variant) {
+  return (
+    <div className={stylesheet.buttonCollection} style={parent.style}>
+      {colorList.map(color => (
+        <Button
+          key={color}
+          onClick={clicked}
+          base={parent.base}
+          variant={variant}
+          color={color}
+        >
+          {children}
+        </Button>
+      ))}
+    </div>
+  )
+}
 
-parentList.map((theme) => {
-  storiesOf(`${i18n.Button}/${theme.name}`, module)
-    .add(i18n.Flat, () => buttonAllColors(theme, `${i18n.Button} ${i18n.Flat}`, 'flat'))
-    .add(i18n.Gradient, () => buttonAllColors(theme, `${i18n.Button} ${i18n.Gradient}`, 'gradient'))
-    .add(i18n.Outline, () => buttonAllColors(theme, `${i18n.Button} ${i18n.Outline}`, 'outline'))
-    .add(i18n.Dashed, () => buttonAllColors(theme, `${i18n.Button} ${i18n.Dashed}`, 'dashed'))
-    .add(i18n.Clean, () => buttonAllColors(theme, `${i18n.Button} ${i18n.Clean}`, 'clean'))
-} )
+parentList.forEach((base) => {
+  storiesOf(`${i18n.Button}/Aplicações ${base.name}`, module)
+    .add(i18n.Flat, () =>
+      buttonAllColors(base, `${i18n.Button} ${i18n.Flat}`, 'flat'))
+    .add(i18n.Gradient, () =>
+      buttonAllColors(base, `${i18n.Button} ${i18n.Gradient}`, 'gradient'))
+    .add(i18n.Outline, () =>
+      buttonAllColors(base, `${i18n.Button} ${i18n.Outline}`, 'outline'))
+    .add(i18n.Dashed, () =>
+      buttonAllColors(base, `${i18n.Button} ${i18n.Dashed}`, 'dashed'))
+    .add(i18n.Clean, () =>
+      buttonAllColors(base, `${i18n.Button} ${i18n.Clean}`, 'clean'))
+})
 
 storiesOf(`${i18n.Button}/${i18n.SizesAndShapes}`, module)
 
-  .add(i18n.Iconed, () =>
+  .add(i18n.Iconed, () => (
     <div className={stylesheet.buttonCollection}>
-      {iconList.map(( icon ) =>
+      {iconList.map(icon => (
         <Button key={icon.text} onClick={clicked}>
           {icon.component()}
           {icon.text}
         </Button>
-      )}
+      ))}
     </div>
-  )
+  ))
 
-  .add(i18n.Block, () =>
+  .add(i18n.Block, () => (
     <div className={stylesheet.buttonBlock}>
-      {[1, 2, 3].map(( n ) =>
-        <Button key={"block-" + n} onClick={clicked} style="block">
+      {[1, 2, 3].map(n => (
+        <Button
+          key={`block-${n}`}
+          onClick={clicked}
+          variant="block"
+        >
           {i18n.Button} {i18n.Block}
-        </Button>
+        </Button>)
       )}
     </div>
-  )
+  ))
 
   .add(i18n.Sizes, () =>
-    <div className={stylesheet.buttonColumn}>
-      {sizeList.map(( size ) =>
+    (<div className={stylesheet.buttonColumn}>
+      {sizeList.map(size => (
         <Button key={size} onClick={clicked} size={size}>
           {i18n.Button}
         </Button>
-      )}
-    </div>
+      ))}
+    </div>)
   )
 
-function buttonAllColors (parent, children, style) {
-  return (
-    <div className={stylesheet.buttonCollection} style={parent.style}>
-      {colorList.map(( color ) =>
-        <Button key={color} onClick={clicked} theme={parent.theme} style={style} color={color}>
-          {children}
-        </Button>
-      )}
-    </div>
-  )
-}
