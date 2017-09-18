cbc77ce486b765c7ad823cea7caec9c30241bbe8
Felquis Gimenes <frgformenton@gmail.com>
2017-09-18 17:33:26 -0300

    search-field: add implementation
      @leonero @felquis

---
+++ webapp/stories/Toolbox/SearchField.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+
+import SearchField from '../../src/components/SearchField'
+
+class SearchFieldState extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      value: ''
+    }
+  }
+  render () {
+    return (
+      <SearchField
+        onChange={value => this.setState({ value })}
+        value={this.state.value}
+        disabled={this.props.disabled}
+      />
+    )
+  }
+}
+
+storiesOf('Toolbox', module)
+  .add('SearchField', () => (
+    <div>
+      <div>
+        <p>Input de busca</p>
+        
+        <SearchFieldState />
+      </div>
+      <div>
+        <p>Input desabilitado</p>
+
+        <SearchFieldState disabled />
+      </div>
+    </div>
+  ))
+
