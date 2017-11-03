74cd38256f5fd0f642b4e20ce2932ec82053790c
flima94 <flima@stone.com.br>
2017-11-03 18:12:26 -0200

    Search: Values of options added, installed query-string

--- webapp/src/containers/Search/index.js
+++ webapp/src/containers/Search/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Dropdown from 'react-dropdown'
 import IconSearch from 'react-icons/lib/md/search'
 import style from './style.css'
 
@@ -12,10 +11,12 @@ class RexSearch extends React.Component {
       value: '',
       data: {},
       errors: {},
+      selectValue: 'Key',
     }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSelectChange = this.handleSelectChange.bind(this)
   }
 
   handleChange (event) {
@@ -32,28 +33,25 @@ class RexSearch extends React.Component {
     event.preventDefault()
   }
 
-  render () {
-    const options = [
-      {
-        name: 'Chave do pedido',
-        value: 'Chave do Pedido',
-      },
-      {
-        name: 'CPF',
-        value: 'CPF',
-      },
-    ]
+  handleSelectChange (event) {
+    this.setState({ selectValue: event.target.value })
+    console.log(this.state.selectValue)
+  }
 
+  render () {
     return (
       <div className={style.rexSearch}>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="search">
             <div className={style.search}>
-              <Dropdown
+              <select
                 className={style.dropdown}
-                options={options}
-                placeholder="Escolha o filtro"
-              />
+                value={this.state.selectValue}
+                onChange={this.handleSelectChange}
+              >
+                <option value="Key">Chave do Pedido</option>
+                <option value="CPF">CPF</option>
+              </select>
               <input
                 onChange={this.handleChange}
                 value={this.state.value}
@@ -61,7 +59,7 @@ class RexSearch extends React.Component {
                 placeholder="Busca"
                 type="search"
               />
-              <Link to={'/search/query='.concat(this.state.value)}>
+              <Link to={'/search/?'.concat(this.state.selectValue).concat('=').concat(this.state.value)}>
                 <button className={style.button}>
                   <IconSearch />
                 </button>
